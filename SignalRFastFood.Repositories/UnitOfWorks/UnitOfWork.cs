06bbf2358c6fce7f93b165cde0c3c43547e41e41
Orçun Aşık <asikorcun@gmail.com>
2023-11-28 21:50:37 +0300

    Implementation of IUnitOfWork was performed.

---
+++ SignalRFastFood.Repositories/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using SignalRFastFood.Entities.UnitOfWorks;
+using SignalRFastFood.Repositories.Repositories.EntityFrameworkCore.Context;
+
+namespace SignalRFastFood.Repositories.UnitOfWorks;
+
+public sealed class UnitOfWork : IUnitOfWork
+{
+    private readonly FastFoodDbContext _context;
+
+    public UnitOfWork(FastFoodDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
