cc5d0e946c4ee7da01f4de60955e97545c4cda36
Orçun Aşık <asikorcun@gmail.com>
2023-11-28 19:37:15 +0300

    Fixed PhoneAddress name to PhoneNumber

--- SignalRFastFood.Entities/Models/Booking.cs
+++ SignalRFastFood.Entities/Models/Booking.cs
@@ -5,7 +5,7 @@ public class Booking
     public int Id { get; set; }
     public string NameSurname { get; set; }
     public string Email { get; set; }
-    public string PhoneAddress { get; set; }
+    public string PhoneNumber { get; set; }
     public byte PersonCount { get; set; }
     public DateTime CreatedDate { get; set; }
 }
