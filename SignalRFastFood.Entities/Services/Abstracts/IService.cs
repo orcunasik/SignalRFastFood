0bb6cbc924750839ed61cb636338980dda1f426b
Orçun Aşık <asikorcun@gmail.com>
2023-11-28 21:51:25 +0300

    A generic Base Service was created

---
+++ SignalRFastFood.Entities/Services/Abstracts/IService.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace SignalRFastFood.Entities.Services.Abstracts;
+
+public interface IService<T> where T : class
+{
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IList<T> entites);
+    Task RemoveByIdAsync(int id);
+    Task RemoveAsync(T entity);
+    Task RemoveRangeAsync(IList<T> entities);
+    Task UpdateAsync(T entity);
+
+    Task<T> GetByIdAsync(int id);
+    Task<IList<T>> GetAllAsync();
+    IQueryable<T> GetWhere(Expression<Func<T, bool>> expression);
+}
