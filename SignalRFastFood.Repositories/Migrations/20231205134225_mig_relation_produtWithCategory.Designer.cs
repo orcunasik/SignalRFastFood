7c2baa8ff03ab57e98ee569511dbb0b7d4862876
Orçun Aşık <asikorcun@gmail.com>
2023-12-05 22:10:48 +0300

    Created migration for relationship between Category and Product

--- SignalRFastFood.Repositories/Migrations/20231205134225_mig_relation_produtWithCategory.Designer.cs
+++ SignalRFastFood.Repositories/Migrations/20231205134225_mig_relation_produtWithCategory.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SignalRFastFood.Repositories.Repositories.EntityFrameworkCore.Context;
 
@@ -11,9 +12,11 @@ using SignalRFastFood.Repositories.Repositories.EntityFrameworkCore.Context;
 namespace SignalRFastFood.Repositories.Migrations
 {
     [DbContext(typeof(FastFoodDbContext))]
-    partial class FastFoodDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231205134225_mig_relation_produtWithCategory")]
+    partial class mig_relation_produtWithCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
