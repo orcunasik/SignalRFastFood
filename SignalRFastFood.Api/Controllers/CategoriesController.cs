10ba9ed465f4e8a24b5dc2ee49e274c513eb233c
Orçun Aşık <asikorcun@gmail.com>
2023-12-05 16:31:28 +0300

    Category Controller has been completed

--- SignalRFastFood.Api/Controllers/CategoriesController.cs
+++ SignalRFastFood.Api/Controllers/CategoriesController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using SignalRFastFood.Core.Models;
 using SignalRFastFood.Core.Services.Abstracts;
+using SignalRFastFood.Dto.CategoryDtos;
 
 namespace SignalRFastFood.Api.Controllers;
 
@@ -9,16 +11,49 @@ namespace SignalRFastFood.Api.Controllers;
 public class CategoriesController : ControllerBase
 {
     private readonly ICategoryService _categoryService;
+    private readonly IMapper _mapper;
 
-    public CategoriesController(ICategoryService categoryService)
+    public CategoriesController(ICategoryService categoryService, IMapper mapper)
     {
         _categoryService = categoryService;
+        _mapper = mapper;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        IList<Category> categories = await _categoryService.GetAllAsync();
-        return Ok(categories);
+        IList<Category> categorys = await _categoryService.GetAllAsync();
+        IList<ResultCategoryDto> categoryDtos = _mapper.Map<IList<ResultCategoryDto>>(categorys);
+        return Ok(categoryDtos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        Category category = await _categoryService.GetByIdAsync(id);
+        GetCategoryDto categoryDto = _mapper.Map<GetCategoryDto>(category);
+        return Ok(categoryDto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(CreateCategoryDto categoryDto)
+    {
+        await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+        return Ok("Kategori Bilgisi Başarı ile Eklendi");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(UpdateCategoryDto categoryDto)
+    {
+        await _categoryService.UpdateAsync(_mapper.Map<Category>(categoryDto));
+        return Ok("Kategori Bilgisi Başarı ile Güncellendi");
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id)
+    {
+        Category category = await _categoryService.GetByIdAsync(id);
+        await _categoryService.RemoveAsync(category);
+        return Ok("Kategori Bilgisi Başarı ile Silinmiştir");
     }
 }
