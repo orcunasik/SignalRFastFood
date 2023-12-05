4da7d9c896bc8359bd8fa6b90a4bc4e440a10b45
Orçun Aşık <asikorcun@gmail.com>
2023-12-05 22:13:21 +0300

    Created dto to display the Category Name when listing products.

---
+++ SignalRFastFood.Dto/ProductDtos/ResultProductWithCategoryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalRFastFood.Dto.ProductDtos;
+
+public class ResultProductWithCategoryDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public decimal Price { get; set; }
+    public bool IsStatus { get; set; }
+    public string CategoryName { get; set; }
+}
