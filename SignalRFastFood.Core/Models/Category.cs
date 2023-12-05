b35c3965a22fdfb446875604bde5e9c381074a3b
Orçun Aşık <asikorcun@gmail.com>
2023-12-05 22:09:36 +0300

    OneToMany relationship between Category and Product

--- SignalRFastFood.Core/Models/Category.cs
+++ SignalRFastFood.Core/Models/Category.cs
@@ -5,4 +5,5 @@ public class Category
     public int Id { get; set; }
     public string Name { get; set; }
     public bool IsStatus { get; set; }
+    public IList<Product> Products { get; set; }
 }
--- SignalRFastFood.Core/Models/Product.cs
+++ SignalRFastFood.Core/Models/Product.cs
@@ -8,4 +8,6 @@ public class Product
     public string ImageUrl { get; set; }
     public decimal Price { get; set; }
     public bool IsStatus { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
 }
